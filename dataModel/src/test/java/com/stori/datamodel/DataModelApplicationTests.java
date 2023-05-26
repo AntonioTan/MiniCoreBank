53923474e83f53bf754f2a8b35ed349554d1a668
frey.ren <frey.ren@storicard.com>
2023-05-26 23:20:54 +0800

    data-model子模块测试用例也可以启动

--- dataModel/src/test/java/com/stori/datamodel/DataModelApplicationTests.java
+++ dataModel/src/test/java/com/stori/datamodel/DataModelApplicationTests.java
@@ -3,10 +3,13 @@ package com.stori.datamodel;
 import com.stori.datamodel.repository.BillRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest
+@SpringBootTest(classes = RunTest.class)
+@RunWith(SpringRunner.class)
 class DataModelApplicationTests {
 
     @Autowired
